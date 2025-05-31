94ea857920541644f8de39888c5816b8cbac2309
Abhilash-Mamidi <146578044+Abhilash-Mamidi@users.noreply.github.com>
2025-05-31 17:15:10 +0530

    Update and rename hex_conv.py to img_to_hex_conv.py

--- img_to_hex_conv.py
+++ img_to_hex_conv.py
@@ -5,13 +5,13 @@ img = Image.open("bmp_24.bmp").convert("RGB")
 width, height = img.size
 pixels = list(img.getdata())
 
-# --- Creating Metadata file ---
+# Creating Metadata file
 with open("image_meta.txt", "w") as meta_file:
     meta_file.write(f"WIDTH={width}\n")
     meta_file.write(f"HEIGHT={height}\n")
     meta_file.write("FORMAT=RGB888\n")
 
-# --- Converting pixel data to hex file ---
+# Converting pixel data to hex file
 with open("image.hex", "w") as hex_file:
     for r, g, b in pixels:
         hex_file.write("{:02X}{:02X}{:02X}\n".format(r, g, b))
